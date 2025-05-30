1e2e9f8ac41ecd224a69563b79532c2b8b2d2901
inlinefun <twenty48fr@gmail.com>
2025-05-30 13:07:28 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import Stuff from './components/Stuff.vue'
+</script>
+
+<template>
+    <Stuff />
+</template>
---
+++ src/components/Stuff.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="backdrop-blur-lg flex h-screen w-screen justify-center items-center">
+        <div class="p-8 bg-gray-900 rounded-2xl flex flex-col items-center">
+            <h1 class="text-4xl font-bold">Hi! I'm inlinefun</h1>
+            <p class="font-medium">I'm a developer and gamer, I enjoy working with Kotlin.</p>
+            <p class="font-medium">
+                I occasionally play games like Minecraft, Geometry Dash and Pokémon.
+            </p>
+        </div>
+    </div>
+</template>
